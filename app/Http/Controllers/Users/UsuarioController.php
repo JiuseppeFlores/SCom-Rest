fb420ff2b75eca625f56457d7c582ed632016a5e
José L. Flores <joseluisfloresquisbert@gmail.com>
2022-09-08 01:59:04 -0400

    Proyecto Base

---
+++ app/Http/Controllers/Users/UsuarioController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuarios = Usuario::all();
+        return $usuarios;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario->ci = $request->ci;
+        $usuario->nombreUsuario = $request->nombreUsuario;
+        $usuario->contraseña = $request->contraseña;
+        $usuario->edad = $request->edad;
+        $usuario->nombre = $request->nombre;
+        $usuario->apellidoPaterno = $request->apellidoPaterno;
+        $usuario->apellidoMaterno = $request->apellidoMaterno;
+        $usuario->estado = $request->estado;
+
+        $usuario->save();
+
+        return $usuario;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+        return $usuario;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $ci)
+    {
+        $usuario = Usuario::findOrFail($ci);
+        $usuario->ci = $request->ci;
+        $usuario->nombreUsuario = $request->nombreUsuario;
+        $usuario->contraseña = $request->contraseña;
+        $usuario->edad = $request->edad;
+        $usuario->nombre = $request->nombre;
+        $usuario->apellidoPaterno = $request->apellidoPaterno;
+        $usuario->apellidoMaterno = $request->apellidoMaterno;
+        $usuario->estado = $request->estado;
+
+        $usuario->save();
+        return $usuario;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($ci)
+    {
+        $usuario = Usuario::destroy($ci);
+        return $usuario;
+    }
+}
