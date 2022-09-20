fd7e5b53855603f1afa084f1005a80537836998a
José L. Flores <joseluisfloresquisbert@gmail.com>
2022-09-19 22:24:12 -0400

    Actualizacion index IngredienteController

--- app/Http/Controllers/Usuarios/UsuarioController.php
+++ app/Http/Controllers/Usuarios/UsuarioController.php
@@ -15,7 +15,7 @@ class UsuarioController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
+    { 
         $usuarios = Usuario::all();
         $data = array('data' => $usuarios,'error' => []);
         return $data;
