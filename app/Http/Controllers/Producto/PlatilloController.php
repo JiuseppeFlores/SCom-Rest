9edc47bcf9797f095406c7ad2b58d0736ce5cc58
Sarai Blanco <saraiblancosalgado2014@gmail.com>
2022-09-25 00:20:18 -0400

    relaciones NyM v2

--- app/Http/Controllers/Producto/PlatilloController.php
+++ app/Http/Controllers/Producto/PlatilloController.php
@@ -58,7 +58,7 @@ class PlatilloController extends Controller
         ->get();
 
         //$platillo -> join($ingredientes)
-        $data = array('data' => $platillo,'ingredientes' => $ingredientes  , 'error' => []);
+        $data = array('data' => [$platillo, $ingredientes ] , 'error' => []);
         return $data;
     }
 
